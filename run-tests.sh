6de0d06891d8e4f0f5fe29bbe030937d856e6562
Uthmanade001 <Adeleyetemilade001@gmail.com>
2025-05-24 15:58:06 +0100

    add real file check test to CI

--- run-tests.sh
+++ run-tests.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-echo "🧪 Running fake tests..."
+echo "🧪 Real Check: Does config.yml exist?"
 
-# Simulated test 1 (pass)
-echo "Test 1: Checking if 2 + 2 == 4"
-if [ $((2 + 2)) -eq 4 ]; then
-  echo "✅ Test 1 passed"
+if [ -f config.yml ]; then
+  echo "✅ config.yml is present"
 else
-  echo "❌ Test 1 failed"
+  echo "❌ config.yml is missing"
   exit 1
 fi
 
-# Simulated test 2 (fail on purpose)
-echo "Test 2: Checking if 5 - 3 == 1"
-if [ $((5 - 3)) -eq 2 ]; then
-  echo "✅ Test 2 passed successfully"
-else
-  echo "❌ Test 2 failed"
-  exit 1
-fi
-
-echo "✅ All tests passeed"
+echo "✅ All checks passed."
